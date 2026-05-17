ea96d6a83f3995f82d44e9a070aa67729f29b15f
sanrivarola <sanrivarola@gmail.com>
2026-05-16 23:25:09 -0300

    Primer commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+   public function dashboard(Request $request)
+   {
+
+      //dd($request->all());
+      //dd($request->get('for-my'));
+      //dd($request->user());
+      //dd($request->user()->id);
+
+      if ($request->get('for-my')) {
+         //$posts = Post::where('user_id', $request->user()->id)->latest()->get();
+         $posts = $request->user()->posts;
+      } else {
+         
+         $posts = Post::latest()->get();
+      }
+
+      return view('dashboard', compact('posts'));
+   }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create(['email' => 's@test.com']);
+        User::factory(19)->hasPosts(4)->create();
+    }
+}
