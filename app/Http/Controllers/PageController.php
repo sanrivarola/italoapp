c774ac2b17fa23b95a5629176f85e3b7e9b086f6
sanrivarola <sanrivarola@gmail.com>
2026-05-17 00:33:17 -0300

    postFriends

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,18 +10,16 @@ class PageController extends Controller
    public function dashboard(Request $request)
    {
 
-      dd(
-         $request->user()->friendsFrom()->get(),
-         $request->user()->friendsTo()->get()
-         );
-      //dd($request->all());
-      //dd($request->get('for-my'));
-      //dd($request->user());
-      //dd($request->user()->id);
 
       if ($request->get('for-my')) {
-         //$posts = Post::where('user_id', $request->user()->id)->latest()->get();
-         $posts = $request->user()->posts()->latest()->get();
+         $user = $request->user();
+
+         $friends_from_ids = $user->friendsFrom()->pluck('users.id');
+         $friends_to_ids = $user->friendsTo()->pluck('users.id');
+         $user_ids = $friends_from_ids->merge($friends_to_ids)->push($user->id);
+
+
+         $posts = Post::whereIn('user_id', $user_ids)->latest()->get();
       } else {
          
          $posts = Post::latest()->get();
